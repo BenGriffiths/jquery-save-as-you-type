f3f51c03eee0f9f9414ac0c61f0404d70ac64d0b
Oliver Georgi <slackero+github@gmail.com>
2017-03-18 16:33:18 +0100

    Options to define functions onSave and onRecover

--- source/sayt.jquery.js
+++ source/sayt.jquery.js
@@ -1,9 +1,6 @@
 /*
- *******************************************************************************
- *
  * sayt - Save As You Type
- *
- *******************************************************************************
+ * =======================
  *
  * This plugin autosaves form input as it is being entered by the
  * user. It is saved to a cookie during each keyup and change.
@@ -12,18 +9,13 @@
  * re-entered for the user by reading the cookie. The cookie can be deleted
  * on the fly by the plugin if required.
  *
- *******************************************************************************
- *
  * Intructions:
  * By: Ben Griffiths, ben@ben-griffiths.com
  * Version: 1.4.5
  *
- * Dependencies:
+ * Dependencies (included):
+ * - jquery-cookie 1.0.0+ (https://github.com/carhartl/jquery-cookie)
  *
- * jquery-cookie 1.0.0 (Relies on the jQuery Cookie plugin https://github.com/carhartl/jquery-cookie)
- * - Included.
- *
- *******************************************************************************
  *
  * Licensed under The MIT License (MIT)
  * you may not use this file except in compliance with the License.
@@ -51,28 +43,28 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- *******************************************************************************
  */
-(function($)
-{
-    $.fn.sayt = function(options)
-    {
+;(function($) {
+
+    $.fn.sayt = function(options) {
+
         /*
          * Get/Set the settings
          */
-        var settings = $.extend(
-        {
-            'prefix'         : 'autosaveFormCookie-',
-            'erase'          : false,
-            'days'           : 3,
-            'autosave'       : true,
-            'savenow'        : false,
-            'recover'        : false,
-            'getformdata'    : false,
-            'autorecover'    : true,
+        var settings = $.extend({
+            'prefix': 'autosaveFormCookie-',
+            'erase': false,
+            'days': 3,
+            'autosave': true,
+            'savenow': false,
+            'recover': false,
+            'getformdata': false,
+            'autorecover': true,
             'checksaveexists': false,
-            'exclude'        : [],
-            'id'             : this.attr('id'),
+            'exclude': [],
+            'id': this.attr('id'),
+            'onRecover': null,
+            'onSave': null
         }, options);
 
         /*
@@ -88,139 +80,119 @@
         /*
          * Erase a cookie
          */
-        if(settings.erase === true)
-        {
-            $.cookie( cookie_id, null);
-            if (typeof(Storage) !== "undefined") {
+        if (settings.erase === true) {
+            $.cookie(cookie_id, null);
+            if (typeof Storage !== 'undefined') {
                 localStorage.removeItem(cookie_id);
             }
-
             return true;
         }
 
         /*
          * Get the forms save cookie (if it has one of course)
          */
-        var autoSavedCookie = (typeof(Storage) !== "undefined") ? localStorage.getItem(cookie_id) : $.cookie(cookie_id);
+        var autoSavedCookie = (typeof Storage !== 'undefined') ? localStorage.getItem(cookie_id) : $.cookie(cookie_id);
 
         /*
          * Check to see if a save exists
          */
-        if(settings.checksaveexists === true)
-        {
+        if (settings.checksaveexists === true) {
             return autoSavedCookie ? true : false;
         }
 
         /*
          * Save form data to a cookie
          */
-        var autoSaveCookie = function(data)
-        {
-            var cookieString = '';
-
-            jQuery.each(data, function(i, field)
-            {
-                cookieString = cookieString + field.name + ':::--FIELDANDVARSPLITTER--:::' + field.value + ':::--FORMSPLITTERFORVARS--:::';
-            });
-
-            if (typeof(Storage) !== "undefined") {
-                localStorage.setItem(cookie_id, cookieString);
-            }
-            else {
-                $.cookie(cookie_id, cookieString, { expires: settings.days });
-            }
-        };
+        var autoSaveCookie = function(data) {
+                var cookieString = '';
+                jQuery.each(data, function(i, field) {
+                    cookieString = cookieString + field.name + ':::--FIELDANDVARSPLITTER--:::' + field.value + ':::--FORMSPLITTERFORVARS--:::';
+                });
+                if (typeof Storage !== 'undefined') {
+                    localStorage.setItem(cookie_id, cookieString);
+                } else {
+                    $.cookie(cookie_id, cookieString, {
+                        expires: settings.days
+                    });
+                }
+                if(typeof settings.onSave === 'function') {
+                    settings.onSave.call(this);
+                }
+            };
 
         /*
          * strpos - equiv to PHP's strpos
          */
-        var strpos = function(haystack, needle, offset)
-        {
-            var i = (haystack+'').indexOf(needle, (offset || 0));
-            return i === -1 ? false : i;
-        };
+        var strpos = function(haystack, needle, offset) {
+                var i = (haystack + '').indexOf(needle, (offset || 0));
+                return i === -1 ? false : i;
+            };
 
         /*
          * Serialize the form data, omit excluded fields marked with data-sayt-exclude attribute.
          */
-        var getFormData = function(theform, excludeSelectors)
-        {
-            //
-            // This is here because jQuery's clone method is basically borked.
-            //
-            // Once they fix that, we'll put it back.
-            //
-            var exclude = '[data-sayt-exclude]';
-
-            if(Array.isArray(excludeSelectors)) {
-                exclude += excludeSelectors.length > 0 ? ', ' + excludeSelectors.join(', ') : '';
-            }
-
-            return $(':not(' + exclude + ')', theform).serializeArray();
-        };
+        var getFormData = function(theform, excludeSelectors) {
+                //
+                // This is here because jQuery's clone method is basically borked.
+                //
+                // Once they fix that, we'll put it back.
+                //
+                var exclude = '[data-sayt-exclude]';
+                if (Array.isArray(excludeSelectors)) {
+                    exclude += excludeSelectors.length > 0 ? ', ' + excludeSelectors.join(', ') : '';
+                }
+                return $(':not(' + exclude + ')', theform).serializeArray();
+            };
 
         /*
          * Perform a manual save
          */
-        if(settings.savenow === true)
-        {
-            autoSaveCookie( getFormData(theform, settings.exclude) );
-
+        if (settings.savenow === true) {
+            autoSaveCookie(getFormData(theform, settings.exclude));
             return true;
         }
 
         /*
          * Recover the form info from the cookie (if it has one)
          */
-        if(settings.autorecover === true || settings.recover === true)
-        {
-            if(autoSavedCookie)
-            {
+        if (settings.autorecover === true || settings.recover === true) {
+            if (autoSavedCookie) {
                 var newCookieString = autoSavedCookie.split(':::--FORMSPLITTERFORVARS--:::'),
                     field_names_array = {};
 
-                $.each(newCookieString, function(i, field)
-                {
+                $.each(newCookieString, function(i, field) {
                     var fields_arr = field.split(':::--FIELDANDVARSPLITTER--:::');
-
-                    if($.trim(fields_arr[0]) !== '')
-                    {
-                        if($.trim(fields_arr[0]) in field_names_array)
-                        {
+                    if ($.trim(fields_arr[0]) !== '') {
+                        if ($.trim(fields_arr[0]) in field_names_array) {
                             field_names_array[$.trim(fields_arr[0])] = (field_names_array[$.trim(fields_arr[0])] + ':::--MULTISELECTSPLITTER--:::' + fields_arr[1]);
-                        }
-                        else
-                        {
+                        } else {
                             field_names_array[$.trim(fields_arr[0])] = fields_arr[1];
                         }
                     }
                 });
 
-                $.each(field_names_array, function(key, value)
-                {
-                    if(strpos(value, ':::--MULTISELECTSPLITTER--:::') > 0)
-                    {
+                $.each(field_names_array, function(key, value) {
+                    if (strpos(value, ':::--MULTISELECTSPLITTER--:::') > 0) {
                         var tmp_array = value.split(':::--MULTISELECTSPLITTER--:::');
-
-                        $.each(tmp_array, function(tmp_key, tmp_value)
-                        {
+                        $.each(tmp_array, function(tmp_key, tmp_value) {
                             // added $(theform) to selectors
-                            $('input[name="' + key + '"], select[name="' + key + '"], textarea[name="' + key + '"]', $(theform) ).find('[value="' + tmp_value + '"]').prop('selected', true);
-                            $('input[name="' + key + '"][value="' + tmp_value + '"], select[name="' + key + '"][value="' + tmp_value + '"], textarea[name="' + key + '"][value="' + tmp_value + '"]', $(theform) ).prop('checked', true);
+                            $('input[name="' + key + '"], select[name="' + key + '"], textarea[name="' + key + '"]', $(theform)).find('[value="' + tmp_value + '"]').prop('selected', true);
+                            $('input[name="' + key + '"][value="' + tmp_value + '"], select[name="' + key + '"][value="' + tmp_value + '"], textarea[name="' + key + '"][value="' + tmp_value + '"]', $(theform)).prop('checked', true);
                         });
-                    }
-                    else
-                    {
-                        $('input[name="' + key + '"], select[name="' + key + '"], textarea[name="' + key + '"]', $(theform) ).val([value]);
+                    } else {
+                        $('input[name="' + key + '"], select[name="' + key + '"], textarea[name="' + key + '"]', $(theform)).val([value]);
                     }
                 });
+            }
+
+            if(typeof settings.onRecover === 'function') {
+                settings.onRecover.call(this);
             }
 
             /*
              * if manual recover action, return
              */
-            if(settings.recover === true)
-            {
+            if (settings.recover === true) {
                 return true;
             }
         }
@@ -228,29 +200,25 @@
         /*
          * Return form data
          */
-        if(settings.getformdata === true)
-        {
+        if (settings.getformdata === true) {
             return getFormData(theform, settings.exclude);
         }
 
         /*
          * Autosave - on typing and changing
          */
-        if(settings.autosave === true)
-        {
-            this.find('input, select, textarea').each(function()
-            {
+        if (settings.autosave === true) {
+            this.find('input, select, textarea').each(function() {
                 $(this).on({
-                    change: function()
-                    {
-                        autoSaveCookie( getFormData(theform, settings.exclude) );
+                    change: function() {
+                        autoSaveCookie(getFormData(theform, settings.exclude));
                     },
-                    keyup: function()
-                    {
-                        autoSaveCookie( getFormData(theform, settings.exclude) );
+                    keyup: function() {
+                        autoSaveCookie(getFormData(theform, settings.exclude));
                     }
                 });
             });
         }
     };
+
 })(jQuery);
